1b89771e092d5b6f205b0e786b12a83363aab547
Mikael Weaver <mikael@mikael.dev>
2022-02-10 15:17:27 -0800

    Fixing colemak

--- scripts/Colemak.ps1
+++ scripts/Colemak.ps1
@@ -1,3 +1,4 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-choco install -y colemak
+Invoke-WebRequest -Uri "https://colemak.com/pub/windows/Colemak-1.1-Caps-Lock-Unchanged.zip" -OutFile $HOME\Downloads\colemak.zip
+mkdir $HOME\Downloads\colemak
+Expand-Archive -LiteralPath $HOME\Downloads\colemak.zip -DestinationPath $HOME\Downloads\colemak
+& $HOME\Downloads\colemak\setup.exe /a
--- scripts/MiscApps.ps1
+++ scripts/MiscApps.ps1
@@ -1,4 +1,4 @@
-choco install -y ditto
+#choco install -y ditto
 choco install -y autohotkey
 choco install -y greenshot
 choco install -y zoomit
