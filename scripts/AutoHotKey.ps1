1aff95af6ba6ae150d8301ad86f94412a7cf038c
Mikael Weaver <mikael@mikael.dev>
2022-02-10 15:40:51 -0800

    Fixed autohotkey path

--- scripts/AutoHotKey.ps1
+++ scripts/AutoHotKey.ps1
@@ -1,11 +1,12 @@
 mkdir $HOME\bin
+mkdir $HOME\bin\AutoHotKey
 
 # Fetch files from GitHub
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mikaelweave/windows-setup/master/AutoHotKey/AlwaysOnTop.ahk" -OutFile $HOME\bin\AlwaysOnTop.ahk
-Invoke-WebRequest -Uri "https://gist.githubusercontent.com/endolith/876629/raw/8e237cf3ed79f33e85af21b4823ffe3d6685161f/AutoCorrect.ahk" -OutFile $HOME\bin\AutoCorrect.ahk
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mikaelweave/windows-setup/master/AutoHotKey/Main.ahk" -OutFile $HOME\bin\Main.ahk
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mikaelweave/windows-setup/master/AutoHotKey/text_replace.ico" -OutFile $HOME\bin\text_replace.ico
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mikaelweave/windows-setup/master/AutoHotKey/startup.cmd" -OutFile $HOME\bin\startup.cmd
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mikaelweave/windows-setup/master/AutoHotKey/AlwaysOnTop.ahk" -OutFile $HOME\bin\AutoHotKey\AlwaysOnTop.ahk
+Invoke-WebRequest -Uri "https://gist.githubusercontent.com/endolith/876629/raw/8e237cf3ed79f33e85af21b4823ffe3d6685161f/AutoCorrect.ahk" -OutFile $HOME\bin\AutoHotKey\AutoCorrect.ahk
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mikaelweave/windows-setup/master/AutoHotKey/Main.ahk" -OutFile $HOME\bin\AutoHotKey\Main.ahk
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mikaelweave/windows-setup/master/AutoHotKey/text_replace.ico" -OutFile $HOME\bin\AutoHotKey\text_replace.ico
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mikaelweave/windows-setup/master/AutoHotKey/startup.cmd" -OutFile $HOME\bin\AutoHotKey\startup.cmd
 
 # Make the startup script...well..startup automatically
 $WScriptShell = New-Object -ComObject WScript.Shell
--- scripts/Colemak.ps1
+++ scripts/Colemak.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest -Uri "https://colemak.com/pub/windows/Colemak-1.1-Caps-Lock-Unchanged.zip" -OutFile $HOME\Downloads\colemak.zip
 mkdir $HOME\Downloads\colemak
-Expand-Archive -LiteralPath $HOME\Downloads\colemak.zip -DestinationPath $HOME\Downloads\colemak
+Expand-Archive -LiteralPath $HOME\Downloads\colemak.zip -DestinationPath $HOME\Downloads\colemak -Force
 & $HOME\Downloads\colemak\setup.exe /a
